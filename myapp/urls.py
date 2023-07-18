417ec538d3fd87ace8e211f5261976b529287ad4
Yeicot JJ Sarmiento Polo <yeicotsarmientopolo2@gmail.com>
2023-07-18 02:09:38 -0500

    Lectura de Unidades por API
    -lectura de api de unidades (solo Campo verde 2 de momento)
    -pequeñas mejoras de rendimiento y css

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name="index"),
+    path('proyectos/', views.proyectos, name="proyectos"),
+    path('unidades/', views.unidades, name="unidades"),
+    path('proyectos/crearProyectos/', views.crearProyecto, name="crearPro"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+def proyectos(request):
+    return render(request,'proyectos.html')
+def crearProyecto(request):
+    return render(request,'crearproyecto.html')
+def unidades(request):
+    return render(request,'unidades.html')
 
 # Create your views here.
